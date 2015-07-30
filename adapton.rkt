6e837be440ff0641fd6423d5b0b8170fb9e24f7e
cmentzer <cwolfmentzer@gmail.com>
2015-07-30 04:29:58 -0400

    added comments and tests to improve understandability for a new user

--- adapton.rkt
+++ adapton.rkt
@@ -1,4 +1,10 @@
 #lang racket
+
+;; This file contains the core of adapton, consisting of:
+;; - definition of functions memoized for adapton
+;; - creation of nodes when those functions are called
+;; - force for nodes and cells
+
 (require (prefix-in r: (only-in racket delay force equal-hash-code))
          racket/format
          rackunit
@@ -12,14 +18,51 @@
 ;; ========================================
 
 ;; define/memo creates a memoized version of a function
+;; any function that you want to be tracked by adapton should use define/memo.
+
+;; tests for define/memo
+#;
+(module+ test
+  (define/memo (add1 n) ;;return a node that adds 1 to n
+    (+ 1 n))
+  (define/memo (fib n) ;; return a node that computes nth number of fib
+    (cond 
+      [(= n 0) 0]
+      [(= n 1) 1]
+      [else (+ (force (fib (- n 1))) (force (fib (- n 2))))]))
+  
+  (check-equal? (node? (add1 4)) #t)
+  (check-equal? (force (add1 4)) 5)
+  (check-equal? (node? (fib 4)) #t)
+  (check-equal? (force (fib 4)) 3))
+
+;; definition
 (define-syntax-rule
   (define/memo (f x ...) e ...)
   (define f
     (matt (λ (x ...) e ...))))
 
-;; the matt structure consists of a function and its memo table
-;; matt structures can be applied like functions on a list of arguments
-;; "xs"
+;; ========================================
+
+;; a matt structure is created when a memoized function is called,
+;; and invokes memo to create a thunk delaying the evaluation of 
+;; that function with the given arguments.
+
+;; tests for matt structure and memo function
+(module+ test
+  (define m1 (matt (λ (a) a)))
+  (define m2 (matt (λ (a b) (+ a b))))
+  
+  ;;ensure that the nodes are well-formed
+  (check-equal? (node? (m1 21)) #t)
+  (check-equal? (force (m1 21)) 21)
+  (check-equal? (node? (m2 21 21)) #t)
+  (check-equal? (force (m2 21 21)) 42)
+  
+  ;;ensure they are added to the memo-table properly
+  (check-equal? (node? (hash-ref *memo-table* (node-id (m1 21)))) #t)
+  (check-equal? (node? (hash-ref *memo-table* (node-id (m2 21 21)))) #t))
+
 (struct matt (f)
   #:property
   prop:procedure 
@@ -32,8 +75,8 @@
     [(matt f)
      (let ([id (equal-hash-code (cons f xs))])
        (write-to-graph (format "[change]add node~n[node ~a red]~n" id))
-       ; (printf "memo table has ~a elements~n" (hash-count *memo-table*))
-       ; (write-to-graph (format "[change]add create edge~n[edge ~a ~a blue]~n" (car (unbox create-stack)) id))
+       ; (write-to-graph (format "[change]add create edge~n[edge ~a ~a blue]~n"
+       ; (car (unbox create-stack)) id))
        (hash-ref! *memo-table* 
                   (equal-hash-code (cons f xs))
                   (node (equal-hash-code (cons f xs))
@@ -44,10 +87,20 @@
                         (λ () (apply f xs))
                         '())))]))
 
+
+;; An articulation point is one of 
+;;    | node
+;;    | cell
+
 ;; forcing an articulation point computes its result.
-;; for nodes, the result is computer and stored in the node
-;; structure. For cells, the result is unboxed. Force also
-;; keeps track of successors and predecessors
+;; for nodes, the result is computed and stored in the node structure.
+;; For cells, the result is the value contained in the cell's box.
+
+;; Force also keeps track of the relationships between nodes and cells,
+;; building the DCG each time an articulation point is forced. We use
+;; a stack to keep track of which AP is currently being forced, and 
+;; leverage the information stored in that stack to update predecessors
+;; and successors for each AP.
 (define (force a)
   (cond
     [(node? a)
@@ -68,11 +121,17 @@
                ;; if there exits a node below this node on the stack, add this node to 
                ;; that node's successors and that node to this node's predecessors 
                (when (not (empty? (cdr (unbox stack))))
-                 (node-update *memo-table* (car (cdr (unbox stack))) "successors" (edge 'n
-                                                                                        (node-id a)
-                                                                                        result))
-                 (write-to-graph (format "[change]add edge~n[edge ~a ~a]~n" (car (cdr (unbox stack))) (node-id a)))
-                 (node-update *memo-table* (node-id a) "predecessors" (car (cdr (unbox stack))))) 
+                 (node-update *memo-table* 
+                              (car (cdr (unbox stack))) 
+                              "successors" 
+                              (edge 'n
+                                    (node-id a)
+                                    result))
+                 (write-to-graph (format "[change]add edge~n[edge ~a ~a]~n" 
+                                         (car (cdr (unbox stack))) 
+                                         (node-id a)))
+                 (node-update *memo-table* (node-id a) 
+                              "predecessors" (car (cdr (unbox stack))))) 
                (set-box! stack (cdr (unbox stack)))
                (set-box! create-stack (cdr (unbox create-stack)))
                result)
@@ -84,11 +143,17 @@
                ;; if there exits a node below this node on the stack, add this node to 
                ;; that node's successors and that node to this node's predecessors
                (when (not (empty? (cdr (unbox stack))))
-                 (node-update *memo-table* (car (cdr (unbox stack))) "successors" (edge 'n
-                                                                                        (node-id a)
-                                                                                        result))
-                 (write-to-graph (format "[change]add edge~n[edge ~a ~a]~n" (car (cdr (unbox stack))) (node-id a)))
-                 (node-update *memo-table* (node-id a) "predecessors" (car (cdr (unbox stack)))))
+                 (node-update *memo-table* 
+                              (car (cdr (unbox stack))) 
+                              "successors" 
+                              (edge 'n
+                                    (node-id a)
+                                    result))
+                 (write-to-graph (format "[change]add edge~n[edge ~a ~a]~n" 
+                                         (car (cdr (unbox stack))) 
+                                         (node-id a)))
+                 (node-update *memo-table* (node-id a) 
+                              "predecessors" (car (cdr (unbox stack)))))
                ;; remove this node from the top of the stack
                (set-box! stack (cdr (unbox stack)))
                (set-box! create-stack (cdr (unbox create-stack)))
@@ -102,16 +167,23 @@
      ;; that node's successors and that node to this cell's predecessors
      (when (and (not (empty? (unbox stack)))
                 (not (empty? (cdr (unbox stack)))))
-       (node-update *memo-table* (car (cdr (unbox stack))) "successors" (edge 'c
-                                                                              (cell-id a) 
-                                                                              '()))
-       (write-to-graph (format "[change]add edge~n[edge ~a ~a]~n" (car (cdr (unbox stack))) (cell-id a)))
+       (node-update *memo-table* 
+                    (car (cdr (unbox stack))) 
+                    "successors" 
+                    (edge 'c
+                          (cell-id a) 
+                          '()))
+       (write-to-graph (format "[change]add edge~n[edge ~a ~a]~n" 
+                               (car (cdr (unbox stack))) 
+                               (cell-id a)))
        (let ([old-cell (hash-ref *cells* (cell-id a))])
-         (hash-set! *cells* (cell-id a) (cell (cell-id a)
-                                              (cell-box old-cell)
-                                              (cons (car (cdr (unbox stack))) 
-                                                    (cell-predecessors old-cell))
-                                              (cell-dirty old-cell)))))
+         (hash-set! *cells* 
+                    (cell-id a) 
+                    (cell (cell-id a)
+                          (cell-box old-cell)
+                          (cons (car (cdr (unbox stack))) 
+                                (cell-predecessors old-cell))
+                          (cell-dirty old-cell)))))
      ;; extract the value of this cell
      (let ([result (unbox (cell-box a))])
        (set-box! stack (cdr (unbox stack)))
