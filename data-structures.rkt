9f4bb522e31129167f597a9d43af463299d46fa7
cmentzer <cwolfmentzer@gmail.com>
2015-07-31 01:22:13 -0400

    added comments and tests

--- data-structures.rkt
+++ data-structures.rkt
@@ -15,37 +15,52 @@
 (define *cells*
   (make-hasheq))
 
-;; stack 
+;; the stack holds a list of articulation points (nodes or cells).
+;; the top element of the stack is the "currently active" articulation
+;; point. the stack is used to keep track of successor/predecessor 
+;; relationships
 (define stack (box '()))
 
-;; stack for create edges
+;; create-stack mirrors the stack but has the element 00000 on the bottom.
+;; when a node is created an edge is created between the new node and its
+;; creator. having a seperate stack for create edges allows us to have 
+;; creation edges between a "user node (00000)" and nodes with no predecessor.
+
+;; create-edges are necessary for purge-table to work properly, and require
+;; the user to run the initial input by creating a thunk and storing it in 
+;; the node 00000. The user then forces the user node to run his or her code.
+
+;; For most of the functionality provided by adapton, this is not necessary.
+;; You should attempt to familiarize yourself with most of the functionality
+;; provided by adapton before worrying about create-edges or user nodes. 
 (define create-stack (box '(00000)))
 
-;; cell counter
+;; cell counter keeps track of the id of the last created cell, and is 
+;; incremented each time a cell is created.
 (define cell-counter (box 0))
 
 ;; ======================== DATA STRUCTURES =====================
 ;; node structure
-(struct node (id dirty successors predecessors creator thunk result) #:transparent)
+(struct node (id dirty successors predecessors creator thunk result))
 
 ;; USER NODE
-;; serves as root node for all user forced nodes
-#;(hash-set! *memo-table* 00000 (node 00000 
-                                    #f 
-                                    '() 
-                                    '() 
-                                    0 
-                                    (λ () 
-                                      (get-unforced-list-from-mergesort
-                                       (force (merge-sort (build-input 8))))) '()))
+;; serves as root node for all user forced nodes, see above
+#;(hash-set! *memo-table* 00000 
+             (node 00000 
+                   #f 
+                   '() 
+                   '() 
+                   0 
+                   (λ () 
+                     (get-unforced-list-from-mergesort
+                      (force (merge-sort (build-input 8))))) '()))
 ;(write-to-graph (format "[change]add node~n[node ~a red]~n" 00000))
 
 ;; edge structure
 ;; type is one of 'n (node) or 'c (cell),
 ;; id is the target of the edge
-;; result is the this edge's knowledge of the old result
-;; of its target.
-(struct edge (type id result) #:transparent)
+;; result is the this edge's knowledge of the old result of its target.
+(struct edge (type id result))
 
 ;; cell structure
-(struct cell (id box predecessors dirty) #:transparent)
+(struct cell (id box predecessors dirty))
--- graphing.rkt
+++ graphing.rkt
@@ -7,8 +7,20 @@
 (provide (all-defined-out))
 
 
+;; There is code throughout the repo that builds a visual representation 
+;; of the DCG of your program. Obviously this code will effect performance, 
+;; and so can be turned off by setting the parameter "graphing-on" to false 
+;; (#f) in this file. If you choose to leave graphing enabled, 
+;; the results of the graphing are automatically stored in the file graph.gmv 
+;; in the same directory as this file. You can modify the location of the 
+;; graph flie below.
+
+;; see the readme for more information about graphing.
+
 ;; ============= GRAPH ===============
 
+;; if graphing-on is set to #t, graphing is enabled.
+;; it can be set to #f to disable all graph-related functions
 (define graphing-on #f)
 
 (when graphing-on
@@ -17,6 +29,11 @@
 
 (when (not graphing-on)
   (displayln "graphing is OFF"))
+
+;; ===================================================================
+
+;; "file" contains the location of the file your graph is stored in,
+;; default is the same location as this file. 
 
 (define file (string-append (~a (current-directory)) "graph.gmv"))
 (when graphing-on
@@ -33,6 +50,8 @@
                      #:exists 'append)))
 
 (when graphing-on (write-to-graph (format "[state]make red~n")))
+
+;; ===================================================================
 
 ;; drop successors takes a node and updates that node's edges on the graph
 ;; drop successors is called when a node is cleaned and must be re-evaluated,
